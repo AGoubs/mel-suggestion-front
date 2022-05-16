65e006c729ce968d394445a404966a86c58efce5
AGoubs <arnaud@goubier.fr>
2022-05-16 13:57:31 +0200

    Restore

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-import VModal from 'vue-js-modal'
-import './index.css'
-import 'flowbite';
-
-Vue.use(VModal)
-=======
->>>>>>> parent of 89747ab... update
 
 Vue.config.productionTip = false
 
