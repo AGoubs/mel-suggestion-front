ddcc393971e515f9987d8d085f011f5b73f8bf84
AGoubs <arnaud@goubier.fr>
2022-05-16 11:48:10 +0200

    Revert "update"
    This reverts commit 89747abe8c2cbdbf03551b63027c519709bbeda5.

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   devServer: {
-    host: '127.0.0.1',
-    port: 8080,
-    public: 'localhost:8080',
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:8000/',
-        changeOrigin: true,
-        logLevel: 'debug',
-        // pathRewrite: { '^/api': '/' },
-      }
-    }
+      host: '127.0.0.1',
+      port: 8080,
+      public: 'localhost:8080',
   }
 }
